94203a5fd6b5b4e27cdb435ce8d2376d2350320e
aidan <as397@st-andrews.ac.uk>
2018-01-24 12:24:19 +0000

    fixing bugs and improving

--- sysclass.py
+++ sysclass.py
@@ -5,7 +5,7 @@ Created on Fri Jan 19 22:56:29 2018
 
 @author: aidan
 """
-from numpy import array, expand_dims, kron, eye, dot, append, ones, arange, outer, zeros
+from numpy import array, expand_dims, kron, eye, dot, append, ones, outer, zeros, shape
 from time import time
 from mpmath import exp
 from scipy.linalg import expm
@@ -19,11 +19,13 @@ class temposys(object):
         #initialise system object with hilbert space dimension = hilbert_dim
         self.dim=hilbert_dim
         
-        self.ham=lambda t: eye(self.dim)    #the reduced system hamiltonian is now a function of time
-        self.lindbds=[]                     #list of lindblads and rates: [[rate1,oper1],[rate2,oper2],...]
+        self.ham=lambda t: zeros((self.dim**2,self.dim**2))    #the reduced system hamiltonian is now a function of time
+        self.diss=zeros((self.dim**2,self.dim**2))                    #list of lindblads and rates: [[rate1,oper1],[rate2,oper2],...]
         self.intparam=[]                    #lists of interaction op eigenvalues and bath etas: [[eigs1,eta1],[eigs2,eta2],..]
+        self.intparam2=[]
+        self.ops=[[0,eye(self.dim)]]
         self.state=eye(self.dim)            #reduced system density matrix
-        
+        self.lindbds=[]
         self.dkmax=0                        #maximum length of the mps
         self.dt=0                           #size of timestep
         self.prec=0                         #precision used in svds
@@ -34,65 +36,93 @@ class temposys(object):
         self.dfile='temp'
         self.mps=mps_block(0,0,0)           #blank mps block
         self.mpo=mpo_block(0,0,0)           #blank mpo block
+    
+    def checkdim(self,op_array):
+        if shape(op_array)==(self.dim,self.dim):
+            return 0
+        else:
+            print(op_array)
+            print('input operator has wrong dims: '+str(shape(op_array)))
+            #exit()
+            
+    def corr(self,op_list):
+        return 0
+    
+    def set_state(self,state_array):
+        self.checkdim(state_array)
+        self.state=state_array.reshape(self.dim**2)
         
-    def getcoeffs(self):
-        #goes through eta functions in intparam creating makri coeffs from them
-        for el in self.intparam:
-            #tb is list of values of eta evaulated at integer timestep
-            tb=list(map(el[1],list(arange(0,(self.dkmax+2)*self.dt,self.dt)))) #tb is array of et evaluated at integer timesteps
-            #etab takes finite differences on tb to create coeffs
-            etab=[tb[1]]
-            for jj in range(1,self.dkmax+1): etab.append(tb[jj+1]-2*tb[jj]+tb[jj-1])
-            #if loops evaluates the eta's and coeffs at further timesteps if needed for new quapi
-            if self.mod>self.dkmax:
-                tb=append(tb,array(
-                        map(el[1],list(arange((self.dkmax+2)*self.dt,(self.mod+3)*self.dt,self.dt)))
-                        ))
-                etab=append(etab,array(
-                        map(lambda jj: tb.diff(jj)-tb.diff(self.dkmax-1),range(self.dkmax+1,self.mod+1))
-                        ))
-            #coeffs are appended to element in intparam: [[eigs1,eta1,coeffs1],[eigs2,eta2,coeffs2],...]   
-            el.append(etab)
-        print('coeffs saved')
+    def set_hamiltonian(self,ham_function):
+        self.checkdim(ham_function(0))
+        self.ham=lambda t: -1j*(kron(ham_function(t),eye(self.dim)) - kron(eye(self.dim),ham_function(t).conj()))
+        return 0
+    
+    def add_dissipation(self,lind_list):
+        for el in lind_list:
+            self.checkdim(el[1])
+            self.diss=self.diss+el[0]*(kron(el[1],el[1].conj())
+                -0.5*(kron(dot(el[1].conj().T,el[1]),eye(self.dim))+kron(eye(self.dim),dot(el[1].conj().T,el[1]).T)))
+    
+    def add_bath(self,bath_list):
+        for el in bath_list:
+            self.checkdim(el[0])
+            if self.dt>0: self.intparam.append([el[0].diagonal(),el[1],self.getcoeffs(el[1])])
+            else: self.intparam.append([el[0].diagonal(),el[1],[]])
+            
+   
+    def discretize(self,dt_float):
+        self.dt=dt_float
+        for el in self.intparam: el[2]=self.getcoeffs(el[1])
+    
+    def convergence_params(self,dt_float,dkmax_int,truncprec_int):
+        self.dkmax=dkmax_int
+        self.prec=10**(-0.1*truncprec_int)
+        self.dt=dt_float
+        for el in self.intparam: el[2]=self.getcoeffs(el[1])
+            
+    def getcoeffs(self,eta_function):
+        #tb is list of values of eta evaulated at integer timestep
+        tb=list(map(eta_function,array(range(self.dkmax+2))*self.dt))
+        #etab takes finite differences on tb to create coeffs
+        etab=[tb[1]]+list(map(lambda jj: tb[jj+1]-2*tb[jj]+tb[jj-1],range(1,self.dkmax+1)))
+        #if 
+        if self.mod>self.dkmax:
+            tb=tb+list(map(eta_function,array(range(self.dkmax+2,self.mod+3))*self.dt))             
+            etab=list(etab)+list(map(lambda jj: tb[jj+1]-tb[jj]-tb[self.dkmax]+tb[self.dkmax-1],range(self.dkmax+1,self.mod+2)))
+        return etab
         
+    def sysprop(self,j): 
+        return expm((self.ham(j*self.dt)+self.diss)*self.dt/2).T         
+              
     def itab(self,dk):
         #function to store the influence function I_dk as a table
         
         #initialise matrix
         iffac=ones((self.dim**2,self.dim**2))
-        
         #loop through all baths multiplyin in the corresponding matrix
         for el in self.intparam:
             #picking out the correct eta coeff
-            eta_dk=el[2][dk+(self.point-dk)*int((self.mod and self.point)>self.dkmax)]
+            eta_dk=el[2][dk+(self.point-dk)*int((self.mod and self.point)>self.dkmax and dk>1)]
             #creating arrays of differences and sums of operator eigenvalues
             difs,sums=array([]),array([])
             for s in el[0]: difs,sums=append(difs,s-array(el[0])),append(sums,s+array(el[0]))
             #creating I_dk matrix and multiplying into iffac (couldnt get exp() to work here so used numerical value....)
-            iffac=iffac*2.7182818284590452353602874713527**(outer(eta_dk.real*difs-1j*eta_dk.imag*sums,-difs))
+            iffac=iffac*2.7182818284590452353602874713527**(outer(eta_dk.real*difs+1j*eta_dk.imag*sums,-difs))
         
         if dk==0: return iffac.diagonal() #I_0 is a funtion of one varibale only so is converted to vector
         else: return iffac
-    
-    def sysprop(self,j):
-        #function to create a system propagator over time self.dt/2
-        
-        #we are using the convention where vec(rho) is rows stacked on vertically
-        #and A.rho.B -> Ax(B.T).vec(rho)
-        #first create hamiltonian commutation superoperator
-        liou=-1j*(kron(self.ham(j*self.dt),eye(self.dim)) - kron(eye(self.dim),self.ham(j*self.dt).conj()))
-        #then loop through list of lindblads to create dissipators
-        for li in self.lindbds:
-            liou=liou+li[0]*(kron(li[1],li[1].conj())
-                -0.5*(kron(dot(li[1].conj(),li[1]),eye(self.dim))+kron(eye(self.dim),dot(li[1].conj().T,li[1]).T)))
-        #and we are propagating initial state from the RIGHT     
-        return expm(liou*self.dt/2).T 
     
     def temposite(self,dk):
         #converts 2-leg I_dk table into a 4-leg tempo mpo_site object
         iffac=self.itab(dk)
         #if dk==1 then multiply in system propagator and I_0
-        if dk==1: iffac=iffac*self.itab(0)*dot(self.sysprop(self.point-1),self.sysprop(self.point))
+        if dk==1: 
+            for opel in self.ops: 
+                if opel[0]==self.point:
+                    supop=kron(opel[1],eye(self.dim)).T
+                    iffac=iffac*self.itab(0)*dot(self.sysprop(self.point-1),dot(supop,self.sysprop(self.point)))
+                else:
+                    iffac=iffac*self.itab(0)*dot(self.sysprop(self.point-1),self.sysprop(self.point))
         
         #initialise 4-leg tensor that will become mpo_site and loop through assigning elements
         tab=zeros((self.dim**2,self.dim**2,self.dim**2,self.dim**2),dtype=complex)
@@ -111,27 +141,23 @@ class temposys(object):
         #prepares system to be propagated once params have been set
         self.dat.append([0,self.state])     #store initial state in data
         self.point=1                        #move to first point
-        self.getcoeffs()                    #calculate the makri coeffs
+        #self.getcoeffs()                    #calculate the makri coeffs
         
         #propagte initial state half a timestep with sys prop and multiply in I_0 to get initial 1-leg ADT           
         self.mps.insert_site(0,expand_dims(expand_dims(
-                dot(self.state.reshape(self.dim**2),self.sysprop(0))*self.itab(0)
+                dot(self.state,self.sysprop(0))*self.itab(0)
                                     ,-1),-1))
         
         #append first site to mpo to give a length=1 block
         self.mpo.append_mposite(self.temposite(1))
-    
-    def getstate(self):
-        #function to return the reduced system physical density matrix at time=self.point*self.dt
-        return dot(self.mps.readout3(),self.sysprop(self.point-1)).reshape((self.dim,self.dim))
-    
+
     def prop(self,ksteps=1):
         #propagates the system for ksteps - system must be prepped first
         for k in range(ksteps):
             
             t0=time()
             #find current time physical state and store this in self.dat
-            self.state=self.getstate()
+            self.state=dot(self.mps.readout3(),self.sysprop(self.point-1))
             self.dat.append([self.point*self.dt,self.state])
             #contract and grow the mps using the mpo
             self.mps.contract_with_mpo(self.mpo,prec=self.prec,trunc_mode='accuracy')
@@ -159,9 +185,16 @@ class temposys(object):
         td=[[],[]]
         for da in self.dat:
             td[0].append(da[0])
-            td[1].append(dot(op,da[1]).trace().real)
+            td[1].append(dot(op,da[1].reshape((self.dim,self.dim))).trace().real)
         return td    
-       
+'''
+system=temposys(2)
+system.set_hamiltonian(lambda t: array([[1,3],[6,8]]))
+system.dt=1
+print(system.ham(0))
+print(shape(system.sysprop(2))  )
+print(system.sysprop2(2)) 
+print(system.sysprop(2))      
 
 ########
 ########
@@ -178,26 +211,28 @@ idd=array([[1,0],[0,1]])
 #initialise a 2d tempo system and set params   
 system=temposys(2)
 #hamiltonina - a function of time, in this case constant
-system.ham=lambda t: sigx
+system.ham=lambda t: sigx*0
 #initial state is excited
-system.state=0.5*(idd+sigz)
+system.state=0.5*(idd+sigx)
 #couples to sigma_z with single ohmic bath 
-system.intparam.append([[1,-1],lambda t: eta_all(t,0.2,1.000001,7.5,0,0.5*0.01*10) ])
+system.intparam.append([[1,-1],lambda t: eta_all(t,0.2,1.000001,7.5,0,0.5*0.01*10)])
+system.lindbds.append([0.05*0,sigm])
+system.lindbds.append([0.05*0,sigp])
+#system.ops.append([150,idd])
 #timestep, memory length and truncation precision
-system.dt=0.25
+system.dt=0.15
 system.dkmax=10
-system.prec=0.00001
-
+system.prec=0.000001
+system.mod=20
 #prep the system for propagation
 system.prep()
 #propagate the system 100 timesteps
-system.prop(100)
-   
+system.prop(20)
 #generate data for obseravble sigz and plot 
-dat=system.opdat(sigz)
+dat=system.opdat(sigx)
 plt.plot(dat[0],dat[1])
 plt.show()
-
+'''
      
         
         
