176cc77a435349f775ff33b3b69f4e593b85a944
Peter Kirton <pgk2@st-andrews.ac.uk>
2018-07-24 22:24:13 +0200

    removing unecessary files

--- pytensor/__init__.py
+++
@@ -1,6 +0,0 @@
-__all__ = ["tensor", "sptensor", "sptenmat", "tenmat"]
-
-from pytensor.sptensor import sptensor
-from pytensor.tensor import tensor
-from pytensor.sptenmat import sptenmat
-from pytensor.tenmat import tenmat
